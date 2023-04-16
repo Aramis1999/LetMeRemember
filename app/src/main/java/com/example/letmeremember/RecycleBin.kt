97ca439701e7ea4ce180e1ea26c0e560c97795eb
mariella1402 <mariiellachiicas14@gmail.com>
2023-04-15 21:03:49 -0600

    papelera

---
+++ app/src/main/java/com/example/letmeremember/RecycleBin.kt
@@ -0,0 +1,11 @@
+package com.example.letmeremember
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecycleBin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycle_bin)
+    }
+}
